e4c84e156bf407912ba516fd7865ce2e26365b54
Anish <kirodiwlanish11@gmail.com>
2023-07-29 15:58:25 +0530

    index file updated

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import userRoutes from "./routes/users.js";
 import questionRoutes from "./routes/Questions.js";
 import answerRoutes from "./routes/Answers.js";
 import connectDB from "./connectMongoDb.js";
+import { Mongoose } from "mongoose";
 
 dotenv.config();
 connectDB();
@@ -23,7 +24,13 @@ app.use("/questions", questionRoutes);
 app.use("/answer", answerRoutes);
 
 const PORT = process.env.PORT || 5000;
+const DATABASE_URL = process.env.CONNECTION_URL;
 
-app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
-});
+mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+  }))
+  .catch((err) => console.log(err.message))
+// app.listen(PORT, () => {
+//   console.log(`server running on port ${PORT}`);
+// });
